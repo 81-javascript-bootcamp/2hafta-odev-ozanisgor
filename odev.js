4ba45f161129d4d2b8544841e7449d2528da761b
Ozan İŞGÖR <ozanisgor11@gmail.com>
2021-02-03 15:27:31 +0000

    For the 1st and 3th challenges I implemented two solutions for both bind() and call()/apply(). For the 2nd challenge i had a solution but not %100 sure.

--- odev.js
+++ odev.js
@@ -4,22 +4,35 @@
 
 **/
 
-var car = { 
-    registrationNumber: "GA12345",
-    brand: "Toyota",
+//Solution with bind()*
+var car = {
+  registrationNumber: "GA12345",
+  brand: "Toyota",
 
-    displayDetails: function(){
-        console.log(this.registrationNumber + " " + this.brand);
-    }
-}
+  displayDetails: function () {
+    console.log(this.registrationNumber + " " + this.brand);
+  },
+};
 
-var myCarDetails =  car.displayDetails;
+var myCarDetails = car.displayDetails.bind(car);
 myCarDetails();
 
+//Solution with call()/apply()*
+var car = {
+  registrationNumber: "GA12345",
+  brand: "Toyota",
+
+  displayDetails: function () {
+    console.log(this.registrationNumber + " " + this.brand);
+  },
+};
+
+car.displayDetails.call(car);
 
 /** 
 
-2.name parametresi alan bir isValidName fonksiyonu yazin. Fonksiyon asagidaki kosullarin hepsi saglaniyorsa true, saglanmiyorsa false donmelidir:
+2.name parametresi alan bir isValidName fonksiyonu yazin. 
+Fonksiyon asagidaki kosullarin hepsi saglaniyorsa true, saglanmiyorsa false donmelidir:
 
 name string olmali
 name bos olmamali
@@ -28,10 +41,28 @@ bosluk icerebilir, ancak bosluk haridcindeki isimler en az 2 karakterden olusmal
 **/
 
 function isValidName(name) {
-  /// your code here
+  const regex = /([a-zA-Z]{3,}\s*)+/;
+
+  if (typeof name != "string") {
+    return false;
+  } else if (name == "") {
+    console.log("Name must be filled out");
+    return false;
+  } else if (!regex.test(name)) {
+    console.log("Name must be at least 3 alphanumeric characters");
+    return false;
+    //Sir, I am not %100 sure using "!" is the most reliable way on line 51 to make it false. The regex that i created is covering all the expectations. So inside of the paranthesis become true but in fact we need the results besides that regex matches. To make it work I added "!" on line 51 just before the regex. With this way all the tests return true but I am still not comfortable with "!". That would be great to have a comment from you about this case sir.
+  }
+  return true;
 }
 
-
+console.log(isValidName("Ahmet") === true);
+console.log(isValidName(false) === false);
+console.log(isValidName(null) === false);
+console.log(isValidName(undefined) === false);
+console.log(isValidName("") === false);
+console.log(isValidName("  \t\n") === false);
+console.log(isValidName("X") === false);
 
 /**
 
@@ -40,17 +71,31 @@ function isValidName(name) {
 
 **/
 
+//Solution with call()/apply()*
 const book = {
-  title: 'Brave New World',
-  author: 'Aldous Huxley',
-}
+  title: "Brave New World",
+  author: "Aldous Huxley",
+};
 
 function summary(genre, year) {
   console.log(
-    `${this.title} was written by ${this.author}. It is a ${genre} novel written in ${year}.`,
-  )
+    `${this.title} was written by ${this.author}. It is a ${genre} novel written in ${year}.`
+  );
 }
 
+summary.call(book, "dystopian", 1932);
 
+//Solution with bind()*
+const book = {
+  title: "Brave New World",
+  author: "Aldous Huxley",
+};
 
+function summary(genre, year) {
+  console.log(
+    `${this.title} was written by ${this.author}. It is a ${genre} novel written in ${year}.`
+  );
+}
 
+const bookSummary = summary.bind(book);
+bookSummary("dystopian", 1932);
