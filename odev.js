db761b3ec3ca9b052343740a049fa2a7f6f7380a
Ozan İŞGÖR <ozanisgor11@gmail.com>
2021-02-04 11:26:22 +0000

    tests removed

--- odev.js
+++ odev.js
@@ -59,13 +59,6 @@ function isValidName(name) {
   return true;
 }
 
-console.log(isValidName("Ahmet") === true);
-console.log(isValidName(false) === false);
-console.log(isValidName(null) === false);
-console.log(isValidName(undefined) === false);
-console.log(isValidName("") === false);
-console.log(isValidName("  \t\n") === false);
-console.log(isValidName("X") === false);
 
 /**
 
